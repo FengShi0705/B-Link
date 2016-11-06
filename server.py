68d6f153a7717e4308d588583de57a30e2e278d1
FengShi0705 <fs2813@ic.ac.uk>
2016-11-06 12:19:15 +0000

    final checked ok for inviting users

--- server.py
+++ server.py
@@ -12,7 +12,7 @@ app.secret_key='\x8b\x19\xa1\xb0D\x87?\xc1M\x04\xff\xc8\xbdE\xb1\xca\xe6\x9e\x8d
 
 # Initial Data
 # whole retrievor, use whole database as its own graph
-myRtr=Retrievor.UndirectedG('undirected(fortest)_R+G+SP+C+c+OTHER','fortest','userdata')
+myRtr=Retrievor.UndirectedG('addNodeEdgeDegree_R+rn+GMHM_undirected_alpha0.65_nodeD1.0_total_v3_csvneo4j','total_v3_csvneo4j','userdata')
 #myRtr=Retrievor.UndirectedG('undirected(fortest)_R+G+SP+C+c','fortest','userdata')
 print 'edges: ', len(myRtr.G.edges())
 print 'nodes: ', len(myRtr.G.nodes())
@@ -294,7 +294,7 @@ def HCI_data():
 
     ################## get question content
 
-    questions = userQuestion(myRtr.userSchema,email,3)
+    questions = userQuestion(myRtr.userSchema,email,5)
     if questions.has_key('get_Rel_one'):
         explore = questions['get_Rel_one']
     else:
